ee97b10de84f2ed78489dc9b6e68cc08c0ee9012
michaeljpitcher <mjp22@st-andrews.ac.uk>
2016-12-06 17:03:52 +0000

    Comments

--- V3_Metapop_edge_weighted/v3_Lung_network_metapop_edge_weight.py
+++ V3_Metapop_edge_weighted/v3_Lung_network_metapop_edge_weight.py
@@ -17,7 +17,7 @@ class MetapopulationWeightedNetwork(nx.Graph):
         :param initial_infections: Count for initial infections (as dict, key=node, value=count)
         :param p_transmit: Probability of transmission
         :param p_growth: Probability of growth (if positive) or decay (if negative)
-        :param time_limit:
+        :param time_limit: Limit of timesteps
         """
         # Create a graph
         nx.Graph.__init__(self)
@@ -103,100 +103,158 @@ class MetapopulationWeightedNetwork(nx.Graph):
         else:
             raise Exception, "Invalid label choice"
 
-        # edge labels
+        # Draw edge labels (if required)
         if edge_labels:
             edge_labels = nx.get_edge_attributes(self, 'weight')
             nx.draw_networkx_edge_labels(self, self.positioning, edge_labels=edge_labels)
 
+        # Plot the figure, save it if required
         plt.axis('off')
         plt.title(title)
         plt.show()
         if save_name is not None:
             fig.savefig(save_name + ".png")  # save as png
 
-    def movie(self, filename, interval):
+    def movie(self, filename, interval, title=None):
+        """
+        Creates a movie of the network and disease progression over time
+        :param filename: Filename of the mp4 to save
+        :param interval: Interval for frames
+        :return:
+        """
+        # TODO - poor quality because of blit = True ??
         print "MAKING MOVIE"
+        # Create a figure
         fig = plt.figure(figsize=(10, 10))
         ax = fig.gca()
+        # Set axes size, but hide labels, ticks, grid
         ax.set_xlim([-0.2, 10.2])
         ax.set_ylim([-0.2, 12.2])
         ax.grid(False)
         ax.get_xaxis().set_ticks([])
         ax.get_yaxis().set_ticks([])
+        # Keep a record of where each node is
         node_markers = []
         for n in self.nodes_iter():
+            # Draw a circle. Position is based on self.positioning.
             circle = plt.Circle(self.positioning[n], radius=0.1, zorder=2)  # node markers at top of the z-order
             ax.add_patch(circle)
             node_markers.append({'node': n, 'position': circle})
+        # Draw edges (based on positioning)
         for e in self.edges_iter():
             xs = [self.positioning[e[0]][0], self.positioning[e[1]][0]]
             ys = [self.positioning[e[0]][1], self.positioning[e[1]][1]]
             line = plt.Line2D(xs, ys, zorder=1, color='k')  # edges lower down the z-order
             ax.add_line(line)
+        # Sort timepoints from the data
         timepoints = sorted(self.data.keys())
+        if title is not None:
+            plt.suptitle(title)
 
-        def update_nodes(time):
+        def update_plot_nodes(time):
+            """
+            Update all the nodes on the plot
+            :param time:
+            :return:
+            """
+            # Alter the title to include the new time
             plt.title(str(time))
+            # Get the nodes from their markers
             for marker in node_markers:
+                # Get node index from marker, get count from data
                 node=marker['node']
                 count = self.data[time][node]
                 # TODO - rigged so you can actually see colour change
                 if count >= 20:
                     count = 20.0
+                # Update colour of node
                 colour = (count/20.0,0,0)
                 marker = marker['position']
                 marker.set(color=colour)
 
         def init():
-            update_nodes(0.0)
+            # Set the initial plot
+            update_plot_nodes(0.0)
 
         def frame(i):
-            update_nodes(timepoints[i])
+            # For each frame, update the node
+            update_plot_nodes(timepoints[i])
 
+        # Create a movie
         movie = animation.FuncAnimation(fig, frame, init_func=init, frames=len(self.data), interval=interval, blit=False)
-        #plt.show()
+        # Save it
         movie.save(filename + '.mp4', writer='ffmpeg_file')
 
     def record_data(self):
+        """
+        Save the current state of the system to a new record in the data dictionary
+        :return:
+        """
+        # Create a new record
         self.data[self.timestep] = dict()
+        # Save every node to it with key=node, value=count
         for (n,data) in self.nodes_iter(data=True):
             self.data[self.timestep][n] = data['count']
 
     def update_node(self, node, amendment):
-        # If previous count was 0, the node has just become infected
+        """
+        Update the population count at a node. Will fail if value drops below 0.
+        :param node: The node to update
+        :param amendment: The amount to adjust by (negative to decrease).
+        :return:
+        """
+        # If previous count was 0, the node has just become infected, so add to the infected list
         if self.node[node]['count'] == 0:
             self.infected_nodes.append(node)
 
+        # Update value, check it hasn't become negative, update the maximum value (for displaying purposes)
         self.node[node]['count'] += amendment
         assert self.node[node]['count'] >= 0, "update_node: Count cannot drop below zero"
         self.max_count = max(self.node[n]['count'] for n in self.nodes())
 
-        # If new count is 0, node is no longer infected
+        # If new count is 0, node is no longer infected, so remove from infected list
         if self.node[node]['count'] == 0:
             self.infected_nodes.remove(node)
 
     def update_totals(self):
+        """
+        Update the total counts used for transmission
+        :return:
+        """
         # TODO - ASSUMPTION: transmission is affected by degree
         # TODO - ASSUMPTION: no maximum capacity in node
-        self.total_possible_transmission = 0.0
-        for node in self.infected_nodes:
-            self.total_possible_transmission += self.node[node]['count'] * self.degree(node)
+        # Transmission is sum of (count * degree) for all infected nodes
+        self.total_possible_transmission = sum([self.node[node]['count'] * self.degree(node)
+                                                for node in self.infected_nodes])
+        # Growth is sum of count for all infected nodes
         self.total_bacteria = sum([self.node[n]['count'] for n in self.infected_nodes])
 
     def transitions(self):
+        """
+        Calculate the rates of each of the transitions (count * probability)
+        :return:
+        """
         rate_for_transmit = self.total_possible_transmission * self.rates['p_transmit']
         rate_for_growth = self.total_bacteria * abs(self.rates['p_growth'])
-
-        return [(rate_for_transmit, lambda t: self.transmit()),
-                (rate_for_growth, lambda t: self.growth())]
+        return [(rate_for_transmit, lambda t: self.transmit()), (rate_for_growth, lambda t: self.growth())]
 
     def calculate_dt(self, total):
-        # calculate the timestep delta
+        """
+        Determine amount of time passed before the next transition
+        :param total:
+        :return:
+        """
         x = np.random.random()
         dt = (1.0 / total) * math.log(1.0 / x)
         return dt
 
     def choose_transition(self, total, transitions):
+        """
+        Determine which of the transitions has occurred based on their rates
+        :param total:
+        :param transitions:
+        :return:
+        """
         # calculate which transition happens
         x = np.random.random() * total
         k = 0
@@ -208,7 +266,12 @@ class MetapopulationWeightedNetwork(nx.Graph):
         return k
 
     def transmit(self):
-        # Pick a node
+        """
+        A member moves from one node population to another. Picks a node based on count and degree, then picks a
+        neighbour based on weights of edges from node to neighbour.
+        :return:
+        """
+        # Pick a node, based on count and degree of nodes
         chosen_node = -1
         r = np.random.random() * self.total_possible_transmission
         running_bacteria_total = 0
@@ -218,7 +281,7 @@ class MetapopulationWeightedNetwork(nx.Graph):
                 chosen_node = node
                 break
 
-        # Pick a neighbour
+        # Pick a neighbour, based on edges of weights between node and its neighbours
         weight_total = sum([data['weight'] for _,_,data in self.edges(chosen_node, data=True)])
         r2 = np.random.random() * weight_total
         running_weight_total = 0.0
@@ -230,6 +293,11 @@ class MetapopulationWeightedNetwork(nx.Graph):
                 return
 
     def growth(self):
+        """
+        Pick a population to increase (or decrease) by 1. Based on counts of nodes.
+        :return:
+        """
+        # Pick a node based on its count
         r = np.random.random() * self.total_bacteria
         running_total = 0
         for node in self.infected_nodes:
@@ -242,36 +310,56 @@ class MetapopulationWeightedNetwork(nx.Graph):
                 return
 
     def run(self):
+        """
+        Run the system dynamics. Calculates transition rates and dt. Performs an action, updates the system time and
+        records values in data dictionary. Runs until time limit exceeded or infection dies out.
+        :return:
+        """
         print "RUNNING"
+        # Run until time limit exceeded or infection dies out
         while self.timestep < self.time_limit and len(self.infected_nodes) > 0:
-
+            # Update the counts, use them to calculate transition rates
             self.update_totals()
-
             transitions = self.transitions()
-
+            # Calculate dt and pick a transition
             total = 0.0
             for (r, _) in transitions:
                 total += r
-
             dt = self.calculate_dt(total)
             chosen_function_index = self.choose_transition(total, transitions)
-
-            # perform the transition
+            # Perform the transition
             transitions[chosen_function_index][1](self)
-
+            # Update timestep and record data
             self.timestep += dt
             self.record_data()
 
 
 class LungMetapopulationWeighted(MetapopulationWeightedNetwork):
 
-    def __init__(self, weight_method, infected_nodes, initial_load, p_transmit, p_growth, time_limit=100):
-        edges = [(0, 1), (1, 2), (2, 3), (1, 4)]
+    def __init__(self, weight_method, infections, p_transmit, p_growth, time_limit=100):
+        """
+        A specific version of Metapopulation weighted network, based on the topology of the central airways of human
+        tracheobronchial tree
+        :param weight_method: Method to use for edge weights ("stahler" or "horsfield")
+        :param infections: Count for initial infections (as dict, key=node, value=count)
+        :param p_transmit: Probability of transmission
+        :param p_growth: Probability of growth (if positive) or decay (if negative)
+        :param time_limit: Limit of timesteps
+        """
+        # Add edges
+        # Trachea
+        edges = [(0, 1)]
+        # Main bronchi
+        edges +=  [(1, 2), (2, 3), (1, 4)]
+        # Lobar bronchi
         edges += [(2, 5), (5, 6), (3, 7), (3, 8), (8, 9), (9, 10), (10, 11), (4, 12), (12, 13), (12, 14), (4, 15),
              (15, 16), (16, 17)]
+        # Segmental bronchi
         edges += [(5,18), (6,19), (6,20), (7,21), (7,22), (8,23), (9,24), (10,25), (11,26),(11,27), (13,28), (13,29),
                   (14,30), (14,31), (15,32), (16,33), (17,34),(17,35)]
 
+        # Node positions
+        # TODO - currently done by eye
         positioning = dict()
         positioning[0] = (5, 10)
         positioning[1] = (5, 8)
@@ -310,18 +398,17 @@ class LungMetapopulationWeighted(MetapopulationWeightedNetwork):
         positioning[34] = (8.5, 4.25)
         positioning[35] = (8.5, 3)
 
-        # Pass in weights as 0 here, then override. Allows the use of network functions to find neighbours
+        # Pass in weights as 0 here, then override later. Allows the use of network functions to find neighbours etc.
         weights = dict()
         for e in edges:
             weights[e] = 0.0
-
-        MetapopulationWeightedNetwork.__init__(self, edges, weights, positioning, infected_nodes, initial_load,
+        MetapopulationWeightedNetwork.__init__(self, edges, weights, positioning, infections,
                                                p_transmit, p_growth, time_limit)
-
+        # Origin and terminal nodes (to compute edge weights)
         self.origin = 0
         self.terminal_nodes = [n for n in self.nodes() if self.degree(n) == 1 and n != self.origin]
         self.non_terminal_nodes = [n for n in self.nodes() if self.degree(n) != 1]
-
+        # Calculate edge weights
         if weight_method == 'horsfield':
             self.set_horsfield_weights()
         elif weight_method == 'stahler':
@@ -330,6 +417,11 @@ class LungMetapopulationWeighted(MetapopulationWeightedNetwork):
             raise Exception, "Invalid edge weight choice"
 
     def set_horsfield_weights(self):
+        """
+        Horsfield & Cumming edge weights. Terminal branches order 1. Higher branches have order 1 higher than maximum
+        of daughter branches.
+        :return:
+        """
         order = 1.0
         queued_nodes = []
         for child_node, parent_node in self.edges(self.terminal_nodes):
@@ -343,16 +435,23 @@ class LungMetapopulationWeighted(MetapopulationWeightedNetwork):
             # Check that all child edges have an order (i.e. only parent is unfilled)
             child_orders = [e[2]['weight'] for e in edges if e[2]['weight'] > 0.0]
             assert len(child_orders) == len(self.edges(node)) - 1
+            # Get the one remaining edge which has weight 0 and get the parent node from it
+            parent_node = [e[1] for e in edges if e[2]['weight'] == 0.0][0]
             new_order = max(child_orders) + 1
-            parent_node = [n for n in self.neighbors(node) if n < node][0]
             # Set the new order on the parent edge
             self[node][parent_node]['weight'] = new_order
-            # Add the parent node to the queue
+            # Add the parent node to the queue (if not the origin)
             if parent_node != self.origin:
                 queued_nodes.append(parent_node)
+            # Order the nodes TODO: whats the point of this - may assume that nodes are numerically ordered
             queued_nodes = sorted(set(queued_nodes), reverse=True)
 
     def set_stahler_weights(self):
+        """
+        Stahler edge weights. Terminal branches order 1. Higher branches have order 1 higher than maximum
+        of daughter branches if branches are equal, else has order of maximum daughter branch.
+        :return:
+        """
         order = 1.0
         queued_nodes = []
         for child_node, parent_node in self.edges(self.terminal_nodes):
@@ -370,7 +469,7 @@ class LungMetapopulationWeighted(MetapopulationWeightedNetwork):
                 new_order = max(child_orders) + 1
             else:
                 new_order = max(child_orders)
-            parent_node = [n for n in self.neighbors(node) if n < node][0]
+            parent_node = [e[1] for e in edges if e[2]['weight'] == 0.0][0]
             # Set the new order on the parent edge
             self[node][parent_node]['weight'] = new_order
             # Add the parent node to the queue
@@ -380,6 +479,8 @@ class LungMetapopulationWeighted(MetapopulationWeightedNetwork):
 
 
 if __name__ == '__main__':
-    ln = LungMetapopulationWeighted('horsfield', [2], 200, 0.3, 0.5, 1)
+    infections = dict()
+    infections[2] = 200
+    ln = LungMetapopulationWeighted('horsfield', infections, 0.3, 0.5, 1)
     ln.run()
-    ln.display("TEST")
+    ln.movie("movie",100, "LUNG")
