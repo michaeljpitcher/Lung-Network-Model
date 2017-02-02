4e752308e1ab9f03d129ae5352ef7a3602fffaa9
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-02-02 11:53:11 +0000

    Network with attributes for each patch
    Each patch has a set of attributes, values of which are determined by
    their location. These attributes are then used to calculate the rates
    of transitions

---
+++ v5_Spatial_heterogeneity/LungCompartmentalNetwork.py
@@ -0,0 +1,150 @@
+from CompartmentalMetapopNetwork import *
+
+
+class LungNetwork(CompartmentalMetapopulationNetwork):
+    """
+    CompartmentalMetapopulation model with a specific topology - i.e. that of the central airways of the human lung
+    Also includes methods to weight the edges based on Horsfield/Stahler methods
+    """
+
+    def __init__(self, time_limit, compartments, initial_loads, weight_method='horsfield'):
+
+        # Trachea
+        edges = [(0, 1)]
+        # Main bronchi
+        edges += [(1, 2), (2, 3), (1, 4)]
+        # Lobar bronchi
+        edges += [(2, 5), (5, 6), (3, 7), (3, 8), (8, 9), (9, 10), (10, 11), (4, 12), (12, 13), (12, 14), (4, 15),
+                  (15, 16), (16, 17)]
+        # Segmental bronchi
+        edges += [(5, 18), (6, 19), (6, 20), (7, 21), (7, 22), (8, 23), (9, 24), (10, 25), (11, 26), (11, 27), (13, 28),
+                  (13, 29), (14, 30), (14, 31), (15, 32), (16, 33), (17, 34), (17, 35)]
+
+        edge_weights = {}
+        for e in edges:
+            edge_weights[e] = 0.0
+
+        # Node positions TODO: done by eye
+        pos = dict()
+        pos[0] = (5, 10)
+        pos[1] = (5, 8)
+        pos[2] = (4, 7)
+        pos[3] = (3.5, 5)
+        pos[4] = (6, 6)
+        pos[5] = (3, 8)
+        pos[6] = (2.75, 8.5)
+        pos[7] = (2.5, 5)
+        pos[8] = (4, 4)
+        pos[9] = (3.5, 3)
+        pos[10] = (3, 2.5)
+        pos[11] = (2.5, 2)
+        pos[12] = (7, 7)
+        pos[13] = (7.5, 8)
+        pos[14] = (8, 7)
+        pos[15] = (6.5, 5)
+        pos[16] = (7.5, 4)
+        pos[17] = (8, 3.5)
+        pos[18] = (2.5, 7.5)
+        pos[19] = (2.5, 9)
+        pos[20] = (3, 9)
+        pos[21] = (2, 5.5)
+        pos[22] = (2, 4)
+        pos[23] = (3.5, 4.25)
+        pos[24] = (4, 2)
+        pos[25] = (2.5, 3.25)
+        pos[26] = (1.5, 1)
+        pos[27] = (2.75, 1)
+        pos[28] = (7.25, 8.5)
+        pos[29] = (8, 8.5)
+        pos[30] = (8.5, 7.5)
+        pos[31] = (8.5, 6.5)
+        pos[32] = (7, 5.5)
+        pos[33] = (7.5, 3)
+        pos[34] = (8.5, 4.25)
+        pos[35] = (8.5, 3)
+
+        # Attributes of each patch
+        patch_attributes = self.compute_attributes(pos)
+
+        # Create the network (allows use of networkx functions like neighbours for calculating edge weights)
+        CompartmentalMetapopulationNetwork.__init__(self, 36, edge_weights, time_limit, compartments, initial_loads,
+                                                    patch_attributes, pos)
+
+        # Origin and terminal nodes (to compute edge weights)
+        self.origin = 0
+        self.terminal_nodes = [n for n in self.nodes() if self.degree(n) == 1 and n.id != self.origin]
+        self.non_terminal_nodes = [n for n in self.nodes() if self.degree(n) != 1]
+        # Calculate edge weights
+        self.set_weights(weight_method)
+
+    def set_weights(self, weight_method):
+        """
+        Set edge weights. Terminal branches order 1. Other branches depend on ordering method:
+        Horsfield - parent branch has order 1 greater than maximum of child branches
+        Stahler - parent branch has order 1 greater than child branches if they are equal, else has max of child orders
+        :return:
+        """
+
+        # Get the nodes in an ordered list - gives a list where all child nodes appear in list before parent
+        # So can add the order of an edge as all child edges (those futher from origin) will already have been computed
+
+        # Start with origin node
+        queued_nodes = [self.node_list[self.origin]]
+        ordered_nodes = []
+
+        # Pull a node from queued nodes, add it to the start of ordered nodes and add its neighbours to queued nodes
+        while len(queued_nodes) > 0:
+            node = queued_nodes.pop()
+            ordered_nodes.insert(0, node)
+            queued_nodes += [n for n in self.neighbors(node) if n not in ordered_nodes]
+
+        # Process all nodes
+        while len(ordered_nodes) > 0:
+            # Pull the node from the list
+            node = ordered_nodes.pop(0)
+            # Don't process origin
+            if node.id == self.origin:
+                break
+            # Find the parent edge (should only be one) as edge where weight hasn't already been set
+            parent_edges = [(node1, node2, data) for node1, node2, data in self.edges(node, data=True) if
+                            data['weight'] == 0.0]
+            assert len(parent_edges) == 1
+            # Get the data from parent edge to update
+            parent_edge = parent_edges[0][2]
+            # If the node is terminal, weight is 1.0
+            if node in self.terminal_nodes:
+                parent_edge['weight'] = 1.0
+            else:
+                # Get the child edges (those with weights already set)
+                child_edges = [(node1, node2, data) for node1, node2, data in self.edges(node, data=True) if
+                               data['weight'] > 0.0]
+                # Get the weights
+                child_orders = [data['weight'] for _,_,data in child_edges]
+                # Determine new weight based on method chosen
+                if weight_method == 'horsfield':
+                    new_order = max(child_orders) + 1.0
+                elif weight_method == 'stahler':
+                    if len(set(child_orders)) <= 1:
+                        new_order = max(child_orders) + 1.0
+                    else:
+                        new_order = max(child_orders)
+                else:
+                    raise Exception, "Invalid ordering method: {0}".format(weight_method)
+                # Set the parent weight
+                parent_edge['weight'] = new_order
+
+    def compute_attributes(self, positions):
+        patch_attributes = dict()
+        for node_id in range(36):
+            patch_attributes[node_id] = dict()
+            # TODO - find out specifics
+            patch_attributes[node_id]['ventilation'] = 1.0 / positions[node_id][1]
+            patch_attributes[node_id]['perfusion'] = 1.0 / positions[node_id][1]
+            patch_attributes[node_id]['oxygen_tension'] = patch_attributes[node_id]['ventilation'] / \
+                                                          patch_attributes[node_id]['perfusion']
+        return patch_attributes
+
+    def transitions(self):
+        raise NotImplementedError
+
+
---
+++ v5_Spatial_heterogeneity/TBFastAndSlow.py
@@ -0,0 +1,157 @@
+from LungCompartmentalNetwork import *
+
+
+class TBFastSlowMetapopulationNetwork(LungNetwork):
+
+    def __init__(self, rates, time_limit, initial_loads_fast, initial_loads_slow, weight_method='horsfield'):
+
+        initial_loads = dict()
+        for id in initial_loads_fast:
+            initial_loads[id] = dict()
+            initial_loads[id]['F'] = initial_loads_fast[id]
+        for id in initial_loads_slow:
+            initial_loads[id] = dict()
+            initial_loads[id]['S'] = initial_loads_slow[id]
+
+        # Create the network
+        LungNetwork.__init__(self, time_limit, ['F', 'S'], initial_loads, weight_method)
+
+        # Assert all rates present
+        expected_rates = ['p_translocate_F', 'p_translocate_S', 'p_growth_F', 'p_growth_S', 'p_change_F_to_S',
+                          'p_change_S_to_F']
+        for r in expected_rates:
+            assert r in rates.keys(), "initialise: Rate {0} is not present".format(r)
+        self.rates = rates
+
+        self.total_translocate_f = 0.0
+        self.total_translocate_s = 0.0
+        self.total_f = 0.0
+        self.total_s = 0.0
+        self.total_f_O2 = 0.0
+        self.total_s_O2 = 0.0
+
+    def update_totals(self):
+
+        self.total_translocate_f = sum([node.counts['F'] * self.degree(node) for node in self.nodes()])
+        self.total_translocate_s = sum([node.counts['S'] * self.degree(node) for node in self.nodes()])
+
+        self.total_f = sum([node.counts['F'] for node in self.nodes()])
+        self.total_s = sum([node.counts['S'] for node in self.nodes()])
+
+        self.total_f_O2 = sum([node.counts['F'] * node.attributes['oxygen_tension'] for node in self.nodes()])
+        self.total_s_O2 = sum([node.counts['S'] * node.attributes['oxygen_tension'] for node in self.nodes()])
+
+    def transitions(self):
+
+        self.update_totals()
+
+        rate_for_translocate_f = self.total_translocate_f * self.rates['p_translocate_F']
+        rate_for_translocate_s = self.total_translocate_s * self.rates['p_translocate_S']
+
+        rate_for_growth_f = self.total_f * abs(self.rates['p_growth_F'])
+        rate_for_growth_s = self.total_s * abs(self.rates['p_growth_S'])
+
+        # TODO - check use of O2 tension - prob
+        rate_for_change_f_s = self.total_f_O2 * self.rates['p_change_F_to_S']
+        rate_for_change_s_f = self.total_s_O2 * self.rates['p_change_S_to_F']
+
+        return [(rate_for_translocate_f, lambda t: self.translocate('F')),
+                (rate_for_translocate_s, lambda t: self.translocate('S')),
+                (rate_for_growth_f, lambda t: self.growth('F')),
+                (rate_for_growth_s, lambda t: self.growth('S')),
+                (rate_for_change_f_s, lambda t: self.change('F','S')),
+                (rate_for_change_s_f, lambda t: self.change('S','F'))]
+
+    def translocate(self, type):
+
+        if type == 'F':
+            r = np.random.random() * self.total_translocate_f
+        elif type == 'S':
+            r = np.random.random() * self.total_translocate_s
+        else:
+            raise Exception, "Invalid transmission: {0} compartment not valid".format(type)
+
+        running_total = 0
+        for id in self.node_list:
+            node = self.node_list[id]
+            running_total += node.counts[type] * self.degree(node)
+            if running_total >= r:
+                total_weights = sum(edge['weight'] for _, _, edge in self.edges(node, data=True))
+                r2 = np.random.random() * total_weights
+                running_total_weights = 0
+                neighbour_ids = sorted([n.id for n in self.neighbors(node)])
+                for neighbour_id in neighbour_ids:
+                    neighbour = self.node_list[neighbour_id]
+                    edge = self.edge[node][neighbour]
+                    running_total_weights += edge['weight']
+                    if running_total_weights > r2:
+                        self.update_node(node, type, -1)
+                        self.update_node(neighbour, type, 1)
+                        return
+
+    def growth(self, type):
+
+        if type == 'F':
+            r = np.random.random() * self.total_f
+        elif type == 'S':
+            r = np.random.random() * self.total_s
+        else:
+            raise Exception, "Invalid growth: {0} compartment not valid".format(type)
+
+        running_total = 0
+        for id in self.node_list:
+            node = self.node_list[id]
+            running_total += node.counts[type]
+            if running_total >= r:
+                if type == 'F':
+                    rate = self.rates['p_growth_F']
+                else:
+                    rate = self.rates['p_growth_S']
+                if rate > 0:
+                    self.update_node(node, type, 1)
+                elif rate < 0:
+                    self.update_node(node, type, -1)
+                return
+
+    def change(self, old_type, new_type):
+
+        if old_type == 'F':
+            r = np.random.random() * self.total_f
+        elif old_type == 'S':
+            r = np.random.random() * self.total_s
+        else:
+            raise Exception, "Invalid change: {0} compartment not valid".format(new_type)
+
+        running_total = 0
+        for id in self.node_list:
+            node = self.node_list[id]
+            running_total += node.counts[old_type]
+            if running_total >= r:
+                self.update_node(node, old_type, -1)
+                self.update_node(node, new_type, 1)
+                return
+
+if __name__ == '__main__':
+    rates = {}
+    rates['p_translocate_F'] = 0.1
+    rates['p_translocate_S'] = 0.1
+    rates['p_growth_F'] = 0.1
+    rates['p_growth_S'] = 0.1
+    rates['p_change_F_to_S'] = 0.1
+    rates['p_change_S_to_F'] = 0.1
+
+    limit = 50
+
+    loads_fast = dict()
+    loads_fast[0] = 10
+    loads_fast[1] = 7
+
+    loads_slow = dict()
+    loads_slow[15] = 1
+    loads_slow[16] = 4
+
+    model = TBFastSlowMetapopulationNetwork(rates, limit, loads_fast, loads_slow)
+
+    model.run()
+
+    model.display()
