c28484a137c7ec23d5a278c7b9f972c9fa9784a8
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-02-27 11:25:47 +0000

    Runner loads TB_FS config

--- v6_Coupled/Runner.py
+++ v6_Coupled/Runner.py
@@ -1,4 +1,4 @@
-from Models.Lung.LungLymph import LungLymph
+from Models.TB.TB_FS import *
 import ConfigParser
 
 BPS_POSITIONS = 'BronchopulmonarySegmentPositions'
@@ -17,6 +17,18 @@ for i in node_pos_config.options(BPS_POSITIONS):
 for i in node_pos_config.options(LN_POSITIONS):
     node_positions[int(i)] = tuple([float(a) for a in node_pos_config.get(LN_POSITIONS, i).split(",")])
 
-a = LungLymph(species=[], loads={}, positions=node_positions)
+parameters = dict()
+parameters[FAST_BACTERIA_TO_LOAD] = 10
+parameters[SLOW_BACTERIA_TO_LOAD] = 0
 
-a.display([])
+parameters[P_REPLICATE_FAST] = 0.0
+parameters[P_REPLICATE_SLOW] = 0.0
+parameters[P_MIGRATE_FAST] = 0.0
+parameters[P_MIGRATE_SLOW] = 0.0
+parameters[P_CHANGE_FAST_TO_SLOW] = 0.1
+parameters[P_CHANGE_SLOW_TO_FAST] = 0.0
+
+a = TB_FS(positions=node_positions, parameters=parameters)
+
+a.run(10)
+a.display([BACTERIA_FAST, BACTERIA_SLOW])
