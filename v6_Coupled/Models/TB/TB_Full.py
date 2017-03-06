85f60dfdace552eeafda67d053e3ef8a34ba2022
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-03-06 15:08:02 +0000

    Totals, events for bac replication

--- v6_Coupled/Models/TB/TB_Full.py
+++ v6_Coupled/Models/TB/TB_Full.py
@@ -27,7 +27,8 @@ class TBMetapopulationModel(LungLymphNetwork):
                    MACROPHAGE_REGULAR, MACROPHAGE_INFECTED,
                    T_CELL]
 
-        expected_parameters = []
+        expected_parameters = [P_REPLICATION_BACTERIA_FAST, P_REPLICATION_BACTERIA_SLOW,
+                               P_REPLICATION_BACTERIA_INTRACELLULAR]
 
         for expected_parameter in expected_parameters:
             assert expected_parameter in parameters, "Parameter {0} missing".format(expected_parameter)
@@ -48,11 +49,50 @@ class TBMetapopulationModel(LungLymphNetwork):
         for t in totals_needed:
             self.totals[t] = 0.0
 
+    def update_totals(self):
+        """
+        Update all the total counts to reflect current state of the network
+        :return:
+        """
+
+        self.reset_totals()
+        # Loop through all BPS nodes
+        for node in self.node_list_bps:
+            self.totals[TOTAL_BACTERIA_FAST] += node.subpopulations[BACTERIA_FAST]
+            self.totals[TOTAL_BACTERIA_SLOW] += node.subpopulations[BACTERIA_SLOW]
+            self.totals[TOTAL_BACTERIA_INTRACELLULAR] += node.subpopulations[BACTERIA_INTRACELLULAR]
+        # Loop through all lymph nodes
+        for node in self.node_list_ln:
+            self.totals[TOTAL_BACTERIA_FAST] += node.subpopulations[BACTERIA_FAST]
+            self.totals[TOTAL_BACTERIA_SLOW] += node.subpopulations[BACTERIA_SLOW]
+            self.totals[TOTAL_BACTERIA_INTRACELLULAR] += node.subpopulations[BACTERIA_INTRACELLULAR]
+
     def events(self):
-        pass
+        """
+        List of events and rates.
+        Each item in list is a tuple of (rate, event), where rate = prob * count, event = lambda function of event
+        :return:
+        """
+        events = []
+
+        # Replication events
+        events.append((self.parameters[P_REPLICATION_BACTERIA_FAST] * self.totals[TOTAL_BACTERIA_FAST],
+                       lambda f: self.bacteria_replication(BACTERIA_FAST)))
+        events.append((self.parameters[P_REPLICATION_BACTERIA_SLOW] * self.totals[TOTAL_BACTERIA_SLOW],
+                       lambda f: self.bacteria_replication(BACTERIA_SLOW)))
+        events.append((self.parameters[P_REPLICATION_BACTERIA_INTRACELLULAR] * self.totals[TOTAL_BACTERIA_INTRACELLULAR]
+                       , lambda f: self.bacteria_replication(BACTERIA_INTRACELLULAR)))
+
+        return events
 
     def bacteria_replication(self, metabolism):
+        """
+        A bacterium replicates, creating an identical member of the same metabolism
+        :param metabolism: Fast, slow or intracellular bacterium
+        :return:
+        """
 
+        # Choose random threshold based on relevant counts
         if metabolism == BACTERIA_FAST:
             r = np.random.random() * self.totals[TOTAL_BACTERIA_FAST]
         elif metabolism == BACTERIA_SLOW:
@@ -62,10 +102,12 @@ class TBMetapopulationModel(LungLymphNetwork):
         else:
             raise Exception("Incorrect metabolism specified: {0}".format(metabolism))
 
+        # Count up members of subpopulations until threshold exceeded
         running_total = 0
         for node in self.node_list.values():
-            running_total += node.subpopulation[metabolism]
+            running_total += node.subpopulations[metabolism]
             if running_total > r:
+                # Create a new bacterium of the same metabolism
                 node.update(metabolism, 1)
                 return
 
