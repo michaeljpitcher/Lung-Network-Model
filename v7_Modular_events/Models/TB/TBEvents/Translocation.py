125863cdfe4f360fb872cc26fdc732a037b5daff
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-03-14 10:12:30 +0000

    Amendment to translocation
    Allow downstream lymph transfer based on infection levels

--- v7_Modular_events/Models/TB/TBEvents/Translocation.py
+++ v7_Modular_events/Models/TB/TBEvents/Translocation.py
@@ -3,6 +3,8 @@ __author__ = "Michael J. Pitcher"
 from ...Base.Event import *
 from ...PulmonaryAnatomy.PulmonaryAnatomyClasses import *
 from ..TBClasses import *
+from ...PulmonaryAnatomy.BronchopulmonarySegment import *
+from ...PulmonaryAnatomy.LymphNode import *
 
 
 class Translocate(Event):
@@ -57,23 +59,59 @@ class TranslocateLymphDownstream(Translocate):
     def __init__(self, class_type, probability):
         Translocate.__init__(self, class_type, LYMPHATIC_VESSEL, probability)
 
+    def increment_from_node(self, node, network):
+        edges = [(neighbour, data) for (neighbour, data) in network.get_neighbouring_edges(node, LYMPHATIC_VESSEL) if
+                 data[DIRECTION] != node]
+        return node.subpopulations[self.class_type] * len(edges)
+
     def pick_an_edge(self, chosen_node, network):
         # Only pick edges which flow away from the node
-        edges = [(n, d) for (n, d) in network.get_neighbouring_edges(chosen_node, LYMPHATIC_VESSEL)
-                 if d[DIRECTION] != chosen_node]
+        edges = [(neighbour, data) for (neighbour, data) in network.get_neighbouring_edges(chosen_node,
+                LYMPHATIC_VESSEL) if data[DIRECTION] != chosen_node]
 
         # Should only be one TODO - this might change
         assert len(edges) == 1, "Incorrect number of lymph edges = {0}".format(len(edges))
         return edges[0]
 
 
-class TranslocateLymphUpstream(Translocate):
+class TranslocateLymphToBPS(Translocate):
+    """
+    Transfer from a lymph node to a BPS
+    """
 
     def __init__(self, class_type, probability):
         Translocate.__init__(self, class_type, LYMPHATIC_VESSEL, probability)
 
+    def increment_from_node(self, node, network):
+        if isinstance(node, LymphNode):
+            edges = [(n, d) for (n, d) in network.get_neighbouring_edges(node, LYMPHATIC_VESSEL) if
+                     isinstance(n, BronchopulmonarySegment)]
+            return node.subpopulations[self.class_type] * len(edges)
+        else:
+            return 0
+
     def pick_an_edge(self, chosen_node, network):
-        edges = [(n, d) for (n, d) in network.get_neighbouring_edges(chosen_node, LYMPHATIC_VESSEL)
-                 if d[DIRECTION] == chosen_node]
+        edges = [(neighbour, data) for (neighbour, data) in network.get_neighbouring_edges(chosen_node,
+                    LYMPHATIC_VESSEL) if isinstance(neighbour, BronchopulmonarySegment)]
         index = np.random.randint(0, len(edges))
         return edges[index]
+
+
+class TranslocateLymphToBPSBasedOnInfection(TranslocateLymphToBPS):
+    """
+    Transfer from a lymph node to a BPS, based on infection levels
+    """
+
+    def __init__(self, class_type, probability):
+        TranslocateLymphToBPS.__init__(self, class_type, probability)
+
+    def pick_an_edge(self, chosen_node, network):
+        edges = [(n, d) for (n, d) in network.get_neighbouring_edges(chosen_node, LYMPHATIC_VESSEL)
+                 if d[DIRECTION] == chosen_node and isinstance(n, BronchopulmonarySegment)]
+        total_infection = sum([n.subpopulations[MACROPHAGE_INFECTED] for (n,d) in edges])
+        r = np.random.random() * total_infection
+        running_total = 0
+        for (neighbour, data) in edges:
+            running_total += neighbour.subpopulations[MACROPHAGE_INFECTED]
+            if running_total > r:
+                return neighbour, data
