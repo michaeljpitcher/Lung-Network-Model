053832ab2c5911cb02d4891d75a1621777d95e78
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-03-14 09:54:24 +0000

    Death events

--- v7_Modular_events/Models/TB/TBEvents/Death.py
+++ v7_Modular_events/Models/TB/TBEvents/Death.py
@@ -6,14 +6,19 @@ from ..TBClasses import *
 
 class Die(Event):
 
-    def __init__(self, class_type, probability):
+    def __init__(self, class_type, probability, kill_load=True):
         self.class_type = class_type
+        self.kill_load = kill_load
         Event.__init__(self, probability)
 
     def increment_from_node(self, node, network):
         return node.subpopulations[self.class_type]
 
     def update_network(self, chosen_node, network):
+        if self.class_type == MACROPHAGE_INFECTED and not self.kill_load:
+            bacteria_to_redistribute = int(round(chosen_node.subpopulations[BACTERIA_INTRACELLULAR] /
+                                                 chosen_node.subpopulations[self.class_type]))
+            chosen_node.update(BACTERIA_SLOW, bacteria_to_redistribute)
         chosen_node.update(self.class_type, -1)
 
 
@@ -25,9 +30,11 @@ class DieIntracellularLoad(Die):
     def increment_from_node(self, node, network):
         return node.subpopulations[self.class_type] * node.subpopulations[BACTERIA_INTRACELLULAR]
 
-    def update_network(self, chosen_node, network):
 
-        bacteria_to_redistribute = int(round(chosen_node.subpopulations[BACTERIA_INTRACELLULAR] /
-                                             chosen_node.subpopulations[self.class_type]))
-        chosen_node.update(BACTERIA_SLOW, bacteria_to_redistribute)
-        chosen_node.update(self.class_type, -1)
+class DieTCell(Die):
+
+    def __init__(self, class_type, probability):
+        Die.__init__(self, class_type, probability)
+
+    def increment_from_node(self, node, network):
+        return node.subpopulations[self.class_type] * node.subpopulations[T_CELL]
