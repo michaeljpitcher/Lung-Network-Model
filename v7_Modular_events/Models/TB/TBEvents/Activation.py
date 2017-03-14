e037f6c648789665fb9d7c8e4d46a186f5c4df31
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-03-14 09:50:17 +0000

    Activation events

---
+++ v7_Modular_events/Models/TB/TBEvents/Activation.py
@@ -0,0 +1,33 @@
+__author__ = "Michael J. Pitcher"
+
+from ...Base.Event import *
+from ..TBClasses import *
+
+
+class Activate(Event):
+
+    def __init__(self, macrophage_state, probability):
+        self.macrophage_state = macrophage_state
+        Event.__init__(self, probability)
+
+    def increment_from_node(self, node, network):
+        # Spontaneous activation
+        return node.subpopulations[self.macrophage_state]
+
+    def update_network(self, chosen_node, network):
+        # If infected, destroys any bacteria inside
+        if self.macrophage_state == MACROPHAGE_INFECTED:
+            bacteria_to_destroy = int(round(chosen_node.subpopulations[BACTERIA_INTRACELLULAR] /
+                                            chosen_node.subpopulations[MACROPHAGE_INFECTED]))
+            chosen_node.update(BACTERIA_INTRACELLULAR, -1 * bacteria_to_destroy)
+        chosen_node.update(self.macrophage_state, -1)
+        chosen_node.update(MACROPHAGE_ACTIVATED, 1)
+
+
+class ActivateByTCell(Activate):
+
+    def __init__(self, macrophage_state, probability):
+        Activate.__init__(self, macrophage_state, probability)
+
+    def increment_from_node(self, node, network):
+        return node.subpopulations[self.macrophage_state] * node.subpopulations[T_CELL]
