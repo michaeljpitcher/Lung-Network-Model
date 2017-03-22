35bef263d92c42b611a73d013c6b4a003fb959e4
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-03-22 14:06:32 +0000

    Oxygen change can only happen at BPS

--- v7_Modular_events/Models/PulmonaryAnatomy/PulmonaryEvents/PulmonaryChange.py
+++ v7_Modular_events/Models/PulmonaryAnatomy/PulmonaryEvents/PulmonaryChange.py
@@ -1,16 +1,20 @@
 __author__ = "Michael J. Pitcher"
 
 from ...Base.Events.Change import *
+from ..BronchopulmonarySegment import *
 
 
-class ChangeByOxygen(Change):
+class ChangeByOxygenBPS(Change):
 
     def __init__(self, type_change_from, type_change_to, probability, oxygen_high=True):
         self.oxygen_high = oxygen_high
         Change.__init__(self, type_change_from, type_change_to, probability)
 
     def increment_from_node(self, node, network):
-        if self.oxygen_high:
-            return node.subpopulations[self.class_from] * node.oxygen_tension
+        if isinstance(node, BronchopulmonarySegment):
+            if self.oxygen_high:
+                return node.subpopulations[self.class_from] * node.oxygen_tension
+            else:
+                return node.subpopulations[self.class_from] * (1/node.oxygen_tension)
         else:
-            return node.subpopulations[self.class_from] * (1/node.oxygen_tension)
+            return 0
