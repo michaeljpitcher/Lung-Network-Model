1901a86a4083fc295a3137fa96c4dea357ed2ac2
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-03-22 14:06:44 +0000

    Name change for oxygen change

--- v7_Modular_events/Models/TB/TBEvents/BacteriumChangeMetabolism.py
+++ v7_Modular_events/Models/TB/TBEvents/BacteriumChangeMetabolism.py
@@ -4,11 +4,11 @@ from ...PulmonaryAnatomy.PulmonaryEvents.PulmonaryChange import *
 from ..TBClasses import *
 
 
-class BacteriumChangeMetabolism(ChangeByOxygen):
+class BacteriumChangeMetabolism(ChangeByOxygenBPS):
 
     def __init__(self, bacterium_metabolism_from, bacterium_metabolism_to, probability):
         if bacterium_metabolism_from == BACTERIA_FAST and bacterium_metabolism_to == BACTERIA_SLOW:
             oxygen_high = False
         else:
             oxygen_high = True
-        ChangeByOxygen.__init__(self, bacterium_metabolism_from, bacterium_metabolism_to, probability, oxygen_high)
+        ChangeByOxygenBPS.__init__(self, bacterium_metabolism_from, bacterium_metabolism_to, probability, oxygen_high)
