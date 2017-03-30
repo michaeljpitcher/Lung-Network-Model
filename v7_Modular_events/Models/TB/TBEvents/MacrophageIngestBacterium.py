c780cb2aee991a55c4153bfce2ee09b1a0276e9f
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-03-30 14:53:45 +0100

    Alterations to mac ingesting bac

--- v7_Modular_events/Models/TB/TBEvents/MacrophageIngestBacterium.py
+++ v7_Modular_events/Models/TB/TBEvents/MacrophageIngestBacterium.py
@@ -2,19 +2,34 @@ __author__ = "Michael J. Pitcher"
 
 from ...Base.Events.Die import *
 from ..TBClasses import *
+from ...Base.Events.Change import *
 
 
 class MacrophageIngestBacterium(DieByOtherClass):
-    def __init__(self, bacterium_type, macrophage_type, probability, destroy_load=False):
-        self.destroy_load = destroy_load
+    """
+    A macrophage ingests a bacteria (possibly infecting the macrophage, possibly destroying the bacteria)
+    """
+    def __init__(self, bacterium_type, macrophage_type, probability, destroy_bacteria=False, new_macrophage_type=None):
+        self.destroy_bacterium = destroy_bacteria
+        if new_macrophage_type is None:
+            self.new_macrophage_type = macrophage_type
+        else:
+            self.new_macrophage_type = new_macrophage_type
+
+        if not self.destroy_bacterium and self.new_macrophage_type not in CLASSES_WITH_INTRACELLULAR:
+            raise Exception("Class {0} cannot retain bacteria".format(self.new_macrophage_type))
+
         DieByOtherClass.__init__(self, bacterium_type, macrophage_type, probability)
 
     def update_network(self, chosen_node, network):
-        if not self.destroy_load:
+        DieByOtherClass.update_network(self, chosen_node, network)
+
+        if not self.destroy_bacterium:
             chosen_node.update(BACTERIA_INTRACELLULAR, 1)
-            if self.class_which_kills == MACROPHAGE_REGULAR:
-                chosen_node.update(MACROPHAGE_REGULAR, -1)
-                chosen_node.update(MACROPHAGE_INFECTED, 1)
+
+        if self.new_macrophage_type != self.class_which_kills:
+            chosen_node.update(self.class_which_kills, -1)
+            chosen_node.update(self.new_macrophage_type, 1)
 
 
 class MacrophageDestroysLoad(DieByOtherClass):
