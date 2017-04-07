8a279fb62f7c90d8272267dc33d13c0b708c2b13
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-07 11:44:07 +0100

    Lymph node test

---
+++ v8_ComMeN/Test/Pulmonary/Test_LymphNode.py
@@ -0,0 +1,18 @@
+import unittest
+
+from v8_ComMeN.ComMeN.Pulmonary.Node.LymphNode import *
+
+
+class LymphNodeTestCase(unittest.TestCase):
+    def setUp(self):
+        self.node_id = 0
+        self.compartments = ['a', 'b', 'c']
+        self.position = (5, 6)
+        self.node = LymphNode(self.node_id, self.compartments, self.position)
+
+    def test_something(self):
+        self.assertTrue(isinstance(self.node, Patch))
+
+
+if __name__ == '__main__':
+    unittest.main()
