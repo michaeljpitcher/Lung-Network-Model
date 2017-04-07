0fff784b2e7e3ab6cbb033710a09a188e9ec9c50
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-07 11:25:19 +0100

    Edge weights and lymph fixes

--- v8_ComMeN/ComMeN/Pulmonary/Network/PulmonaryAnatomyNetwork.py
+++ v8_ComMeN/ComMeN/Pulmonary/Network/PulmonaryAnatomyNetwork.py
@@ -11,6 +11,7 @@ from ..PulmonaryClasses import *
 from ..Node.BronchialTreeNode import *
 from ..Node.LymphNode import *
 from ...Base.Network.MetapopulationNetwork import *
+from ...Base.Network.WeightFunctions import *
 
 
 __author__ = "Michael Pitcher"
@@ -24,8 +25,8 @@ __status__ = "Development"
 
 class PulmonaryAnatomyNetwork(MetapopulationNetwork):
 
-    def __init__(self, compartments, events_and_node_types, bronchial_tree_nodes=True, lymphatic_nodes=True,
-                 haematogenous_reseeding=True):
+    def __init__(self, compartments, events_and_node_types, bronchial_tree_nodes=True,
+                 bronchial_tree_weight_method=STAHLER, lymphatic_nodes=True, haematogenous_reseeding=True):
 
         nodes = []
         edges = []
@@ -34,24 +35,30 @@ class PulmonaryAnatomyNetwork(MetapopulationNetwork):
             for id in Data_BronchialTree.BRONCHIAL_TREE_IDS:
                 position = Data_BronchialTree.BRONCHIAL_TREE_POSITIONS[id]
                 ventilation = Data_BronchialTree.ventilation_from_position(position)
-                perfusion = Data_BronchialTree.perfusion_from_positon(position)
-                node = BronchialTreeNode(compartments, ventilation, perfusion, position)
+                perfusion = Data_BronchialTree.perfusion_from_position(position)
+                node = BronchialTreeNode(id, compartments, ventilation, perfusion, position)
                 nodes.append(node)
 
+            edge_weights = tree_weight_calculations(0, Data_BronchialTree.BRONCHIAL_TREE_EDGES,
+                                                    bronchial_tree_weight_method)
+
             for (u,v) in Data_BronchialTree.BRONCHIAL_TREE_EDGES:
-                # TODO - edge weights
-                edge_data = {EDGE_TYPE: BRONCHUS, WEIGHT: 0}
+                if (u,v) in edge_weights:
+                    weight = edge_weights[(u, v)]
+                else:
+                    weight = edge_weights[(v, u)]
+                edge_data = {EDGE_TYPE: BRONCHUS, WEIGHT: weight}
                 edge = (nodes[u], nodes[v], edge_data)
                 edges.append(edge)
 
         if lymphatic_nodes:
             for id in Data_Lymphatic.LYMPH_NODE_IDS:
-                position = Data_BronchialTree.BRONCHIAL_TREE_POSITIONS[id]
-                node = LymphNode(compartments, position)
+                position = Data_Lymphatic.LYMPH_NODE_POSITIONS[id]
+                node = LymphNode(id, compartments, position)
                 nodes.append(node)
 
             for (u,v) in Data_Lymphatic.LYMPH_EDGES:
-                edge_data = {EDGE_TYPE: BRONCHUS, DIRECTION: v}
+                edge_data = {EDGE_TYPE: LYMPHATIC_VESSEL, DIRECTION: v}
                 edge = (nodes[u], nodes[v], edge_data)
                 edges.append(edge)
 
@@ -60,3 +67,4 @@ class PulmonaryAnatomyNetwork(MetapopulationNetwork):
             pass
 
         MetapopulationNetwork.__init__(self, compartments, nodes, edges, events_and_node_types)
+
