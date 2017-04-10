c861a7d49422c318f474268af98d6c2737ac2495
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-10 16:57:01 +0100

    Change by oxygen subclasses change

--- v8_ComMeN/ComMeN/Pulmonary/Events/PulmonaryChange.py
+++ v8_ComMeN/ComMeN/Pulmonary/Events/PulmonaryChange.py
@@ -6,7 +6,7 @@ Long Docstring
 
 """
 
-from ...Base.Events.Event import *
+from ...Base.Events.Change import *
 
 __author__ = "Michael Pitcher"
 __copyright__ = "Copyright 2017"
@@ -17,19 +17,15 @@ __email__ = "mjp22@st-andrews.ac.uk"
 __status__ = "Development"
 
 
-class ChangeByOxygen(Event):
+class ChangeByOxygen(Change):
     def __init__(self, probability, compartment_from, compartment_to, oxygen_high_to_change):
-        self.compartment_from = compartment_from
-        self.compartment_to = compartment_to
         self.oxygen_high_to_change = oxygen_high_to_change
-        Event.__init__(self, probability)
+        Change.__init__(self, probability, compartment_from, compartment_to)
 
     def increment_from_node(self, node, network):
         # TODO - check viability of this method
         if self.oxygen_high_to_change:
-            return node.oxygen_tension
+            oxygen_factor = node.oxygen_tension
         else:
-            return 1-node.oxygen_tension
-
-    def update_node(self, node, network):
-        change(node, self.compartment_from, self.compartment_to)
+            oxygen_factor = 1-node.oxygen_tension
+        return Change.increment_from_node(self, node, network) * oxygen_factor
