a721afe5db31ca451d182a1877c55f6684e0069a
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-11 13:27:11 +0100

    Bronchopulmonary Segment tests

---
+++ v8_ComMeN/Test/Pulmonary/Test_BronchopulmonarySegment.py
@@ -0,0 +1,20 @@
+import unittest
+
+from v8_ComMeN.ComMeN.Pulmonary.Node.BronchopulmonarySegment import BronchopulmonarySegment
+
+
+class BronchopulmonarySegmentTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.comps = ['a','b']
+        self.node_high_O2 = BronchopulmonarySegment(0, self.comps, 0.3, 0.01, (0, 0))
+        self.node_low_O2 = BronchopulmonarySegment(0, self.comps, 0.9, 0.89, (0, 0))
+        self.node_zero_O2 = BronchopulmonarySegment(0, self.comps, 0.9, 0.9, (0, 0))
+
+    def test_initialise(self):
+        self.assertEqual(self.node_high_O2.ventilation, 0.3)
+        self.assertEqual(self.node_high_O2.perfusion, 0.01)
+
+        self.assertEqual(self.node_high_O2.oxygen_tension, 0.3 - 0.01)
+        self.assertEqual(self.node_low_O2.oxygen_tension, 0.9 - 0.89)
+        self.assertEqual(self.node_zero_O2.oxygen_tension, 0.0000000001)
