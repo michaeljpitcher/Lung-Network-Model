fb4a6581ebc584d0c05fe33bd94c6ca93d199a2a
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-11 13:54:13 +0100

    Pulmonary anatomy tests include blood

--- v8_ComMeN/ComMeN/Pulmonary/Network/PulmonaryAnatomyNetwork.py
+++ v8_ComMeN/ComMeN/Pulmonary/Network/PulmonaryAnatomyNetwork.py
@@ -6,7 +6,9 @@ Long Docstring
 
 """
 
-from ..Data import *
+from v8_ComMeN.ComMeN.Pulmonary.Data.Data_BronchialTree import *
+from v8_ComMeN.ComMeN.Pulmonary.Data.Data_Lymphatic import *
+from v8_ComMeN.ComMeN.Pulmonary.Data.Data_HaematogenousReseed import *
 from ..PulmonaryClasses import *
 from ..Node.BronchialTreeNode import *
 from ..Node.BronchopulmonarySegment import *
@@ -33,24 +35,23 @@ class PulmonaryAnatomyNetwork(MetapopulationNetwork):
         edges = []
 
         if bronchial_tree_nodes:
-            for id in Data_BronchialTree.BRONCHIAL_TREE_IDS:
-                position = Data_BronchialTree.BRONCHIAL_TREE_POSITIONS[id]
-                ventilation = Data_BronchialTree.ventilation_from_position(position)
-                perfusion = Data_BronchialTree.perfusion_from_position(position)
+            for id in BRONCHIAL_TREE_NODE_IDS:
+                position = BRONCHIAL_TREE_NODE_POSITIONS[id]
+                ventilation = ventilation_from_position(position)
+                perfusion = perfusion_from_position(position)
                 node = BronchialTreeNode(id, compartments, ventilation, perfusion, position)
                 nodes.append(node)
                 
-            for id in Data_BronchialTree.BRONCHOPULMONARY_SEGMENT_IDS:
-                position = Data_BronchialTree.BRONCHOPULMONARY_SEGMENT_POSITIONS[id]
-                ventilation = Data_BronchialTree.ventilation_from_position(position)
-                perfusion = Data_BronchialTree.perfusion_from_position(position)
+            for id in BRONCHOPULMONARY_SEGMENT_IDS:
+                position = BRONCHOPULMONARY_SEGMENT_POSITIONS[id]
+                ventilation = ventilation_from_position(position)
+                perfusion = perfusion_from_position(position)
                 node = BronchopulmonarySegment(id, compartments, ventilation, perfusion, position)
                 nodes.append(node)
 
-            edge_weights = tree_weight_calculations(0, Data_BronchialTree.BRONCHIAL_TREE_EDGES,
-                                                    bronchial_tree_weight_method)
+            edge_weights = tree_weight_calculations(0, BRONCHIAL_TREE_EDGES, bronchial_tree_weight_method)
 
-            for (u,v) in Data_BronchialTree.BRONCHIAL_TREE_EDGES:
+            for (u,v) in BRONCHIAL_TREE_EDGES:
                 if (u,v) in edge_weights:
                     weight = edge_weights[(u, v)]
                 else:
@@ -60,18 +61,18 @@ class PulmonaryAnatomyNetwork(MetapopulationNetwork):
                 edges.append(edge)
 
         if lymphatic_nodes:
-            for id in Data_Lymphatic.LYMPH_NODE_IDS:
-                position = Data_Lymphatic.LYMPH_NODE_POSITIONS[id]
+            for id in LYMPH_NODE_IDS:
+                position = LYMPH_NODE_POSITIONS[id]
                 node = LymphNode(id, compartments, position)
                 nodes.append(node)
 
-            for (u,v) in Data_Lymphatic.LYMPH_EDGES:
+            for (u,v) in LYMPH_EDGES:
                 edge_data = {EDGE_TYPE: LYMPHATIC_VESSEL, DIRECTION: v}
                 edge = (nodes[u], nodes[v], edge_data)
                 edges.append(edge)
 
         if haematogenous_reseeding:
-            for (u, v) in Data_HaematogenousReseed.HAEMATOGENOUS_EDGES:
+            for (u, v) in HAEMATOGENOUS_EDGES:
                 edge_data = {EDGE_TYPE: HAEMATOGENOUS, DIRECTION: v}
                 edge = (nodes[u], nodes[v], edge_data)
                 edges.append(edge)
