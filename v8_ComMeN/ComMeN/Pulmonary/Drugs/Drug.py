b82b2cff8bf99e58656b9e21ceb1c3d6735db3c8
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-13 13:27:21 +0100

    Drug package

---
+++ v8_ComMeN/ComMeN/Pulmonary/Drugs/Drug.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""Short docstring
+
+Long Docstring
+
+"""
+
+__author__ = "Michael Pitcher"
+__copyright__ = "Copyright 2017"
+__credits__ = ["Michael Pitcher"]
+__license__ = ""
+__version__ = "1.0.8"
+__email__ = "mjp22@st-andrews.ac.uk"
+__status__ = "Development"
+
+# TODO - properties of drugs (effectiveness vs fast/slow, other)
+
+class Drug:
+
+    def __init__(self, name):
+        self.drug_name = name
