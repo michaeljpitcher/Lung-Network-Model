6361f1ff748824452447344770922e03543ed6d1
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-13 12:29:01 +0100

    Test suite

---
+++ v8_ComMeN/Test/TestSuite.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""Short docstring
+
+Long Docstring
+
+"""
+
+import unittest
+
+__author__ = "Michael Pitcher"
+__copyright__ = "Copyright 2017"
+__credits__ = ["Michael Pitcher"]
+__license__ = ""
+__version__ = "1.0.8"
+__email__ = "mjp22@st-andrews.ac.uk"
+__status__ = "Development"
+
+c = unittest.TestSuite()
+
+testmodules = [
+    'Test_Base.Test_change',
+    'Test_Base.Test_Creation',
+    ]
+
+suite = unittest.TestSuite()
+
+for t in testmodules:
+    try:
+        # If the module defines a suite() function, call it to get the suite.
+        mod = __import__(t, globals(), locals(), ['suite'])
+        suitefn = getattr(mod, 'suite')
+        suite.addTest(suitefn())
+    except (ImportError, AttributeError):
+        # else, just load all the test cases from the module.
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+unittest.TextTestRunner().run(suite)
