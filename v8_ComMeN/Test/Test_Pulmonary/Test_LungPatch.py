27de6e077bdba28148f0ff58d15e393ddc4cec46
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-18 11:29:43 +0100

    Test for no drugs

--- v8_ComMeN/Test/Test_Pulmonary/Test_LungPatch.py
+++ v8_ComMeN/Test/Test_Pulmonary/Test_LungPatch.py
@@ -8,9 +8,10 @@ class LungPatchTestCase(unittest.TestCase):
     def setUp(self):
         self.drugs = ['I', 'R', 'P', 'E']
         self.patch = LungPatch(0, ['a','b'], (8,8), self.drugs)
-        self.patch_no_drugs = LungPatch(0, ['a', 'b'], (8, 8), self.drugs)
+        self.patch_no_drugs = LungPatch(0, ['a', 'b'], (8, 8), None)
 
     def test_initialise(self):
         self.assertItemsEqual(self.patch.chemotherapy.keys(), self.drugs)
         for d in self.drugs:
             self.assertEqual(self.patch.chemotherapy[d], 0.0)
+        self.assertFalse(self.patch_no_drugs.chemotherapy.keys())
