2aa72eb19a7013e24d760a99f7d06708065251e2
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-18 11:27:07 +0100

    Chemo is in lung patch

--- v8_ComMeN/ComMeN/Pulmonary/Node/BronchialTreeNode.py
+++ v8_ComMeN/ComMeN/Pulmonary/Node/BronchialTreeNode.py
@@ -29,7 +29,6 @@ class BronchialTreeNode(LungPatch):
             self.oxygen_tension = 0.0000000001
         else:
             self.oxygen_tension = self.ventilation - self.perfusion
-        self.chemotherapy = dict()
 
     def __str__(self):
         return "BronchialTreeNode {" + str(self.node_id) + "}"
