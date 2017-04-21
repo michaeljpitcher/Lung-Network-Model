f52cd0040a4d6b67edae3e9d3c58962e530b1ab0
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-21 13:52:12 +0100

    Full working model

--- v8_ComMeN/ComMeN/TB/Models/TB_Model_Full.py
+++ v8_ComMeN/ComMeN/TB/Models/TB_Model_Full.py
@@ -7,18 +7,14 @@ Long Docstring
 """
 
 from ...Pulmonary.Network.PulmonaryAnatomyNetwork import *
-from ..Events.BacteriaChange import *
-from ..Events.BacteriaReplication import *
-from ..Events.BacteriaTranslocate import *
-from ..Events.MacrophageActivation import *
-from ..Events.MacrophageDeath import *
-from ..Events.MacrophageIngestBacteria import *
-from ..Events.MacrophageRecruitment import *
-from ..Events.MacrophageTranslocate import *
-from ..Events.TCellDeath import *
-from ..Events.TCellRecruitment import *
-from ..Events.TCellTranslocate import *
 from ..TBClasses import *
+import inspect
+import sys
+import os
+
+from ..EventsWithCompartments import *
+
+
 
 __author__ = "Michael Pitcher"
 __copyright__ = "Copyright 2017"
@@ -31,19 +27,42 @@ __status__ = "Development"
 
 class TBModelFull(PulmonaryAnatomyNetwork):
 
-    def __init__(self, seeding, rate_bac_rep_fast):
+    def __init__(self, initial_macrophages_bps, initial_macrophages_tbn, initial_macrophages_lymph,
+                 initial_bacteria_fast, initial_bacteria_slow, probabilities,
+                 include_bronchials, include_lymphatics, include_bloodstream,
+                 tree_weight_method=HORSFIELD):
 
         compartments = [BACTERIA_FAST, BACTERIA_SLOW, BACTERIA_INTRACELLULAR] + \
                        [MACROPHAGE_REGULAR, MACROPHAGE_INFECTED, MACROPHAGE_ACTIVATED] + \
                        [T_CELL]
 
-        events_node_types = dict()
-        # Replication
-        bac_rep_fast = BacteriaReplication(rate_bac_rep_fast, BACTERIA_FAST)
-        events_node_types
+        # TODO - not the nicest code but this does work. Maybe find better way to import (i.e. Event subclasses?)
+        # Get all modules within the EventsWithCompartments directory (based on files ending py
+        event_modules = ['ComMeN.TB.EventsWithCompartments.' + p[0:-3] for p in
+                         os.listdir("ComMeN/TB/EventsWithCompartments") if p.endswith('.py') and p != '__init__.py']
+
+        # Look in each module for classes
+        event_classes = []
+        for module in event_modules:
+            event_classes += inspect.getmembers(sys.modules[module], lambda member: inspect.isclass(member) and
+                                                                                    member.__module__ == module)
+
+        # Using class name and class, add event (class name is probability key)
+        events = []
+        for (prob_key, event_class) in event_classes:
+            if prob_key in probabilities:
+                events.append(event_class(probabilities[prob_key]))
+            else:
+                print "Event {0} probability not defined, skipping".format(prob_key)
+
+        PulmonaryAnatomyNetwork.__init__(self, compartments, events,
+                                         bronchial_tree_nodes=include_bronchials,
+                                         bronchial_tree_weight_method=tree_weight_method,
+                                         lymphatic_nodes=include_lymphatics,
+                                         haematogenous_reseeding=include_bloodstream)
+
+        seeding = dict()
 
 
 
-
-
-        PulmonaryAnatomyNetwork(self.compartments)
+        self.seed_network(seeding)
--- v8_ComMeN/Runner.py
+++ v8_ComMeN/Runner.py
@@ -6,10 +6,8 @@ Long Docstring
 
 """
 
-from ComMeN.Pulmonary.Network.PulmonaryAnatomyNetwork import *
-from ComMeN.TB.EventsWithCompartments.TCellRecruitment import *
-from ComMeN.Pulmonary.Visuals.PulmonaryNetworkGraph import *
-
+from ComMeN.TB.Models.TB_Model_Full import *
+import ConfigParser as cp
 
 __author__ = "Michael Pitcher"
 __copyright__ = "Copyright 2017"
@@ -19,7 +17,33 @@ __version__ = "1.0.8"
 __email__ = "mjp22@st-andrews.ac.uk"
 __status__ = "Development"
 
-event = TCellRecruitmentBronchialRegular(0.1)
-a = PulmonaryAnatomyNetwork(['a','b'],[event], True,HORSFIELD,True,True)
 
-draw_pulmonary_network_graph(a, "TITLE")
+# Load config
+config_file = 'TB_model.config'
+config = cp.ConfigParser()
+# Force config to keep upper case
+config.optionxform = str
+if not config.read(config_file):
+    raise IOError("Configuration file (" + config_file + ") not found")
+
+# Read topology from config
+include_bronchials = config.getboolean("TopologyParameters", "bronchial_tree")
+tree_weight_method = config.get("TopologyParameters", "bronchial_tree_weight_method")
+include_lymphatics = config.getboolean("TopologyParameters", "lymphatics")
+include_bloodstream = config.getboolean("TopologyParameters", "bloodstream")
+
+# Read probabilities from config
+probability_sections = ["BacterialEventProbabilities", "MacrophageEventProbabilities", "TCellEventProbabilities"]
+probabilities = dict()
+for section in probability_sections:
+    for event in config.items(section):
+        probabilities[event[0]] = float(event[1])
+
+seeding = dict()
+print config.items("InitialSeedingMacrophages")
+
+model = TBModelFull(seeding, probabilities, include_bronchials=include_bronchials,
+                     tree_weight_method=tree_weight_method, include_lymphatics=include_lymphatics,
+                     include_bloodstream=include_bloodstream)
+
+model.run(100)
