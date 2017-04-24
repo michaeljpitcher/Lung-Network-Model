8be4747c8020981b92373080914faf688df0962d
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-24 12:40:27 +0100

    T-Cells can clone themselves

---
+++ v8_ComMeN/ComMeN/TB/EventsWithCompartments/TCellCloning.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""Short docstring
+
+Long Docstring
+
+"""
+
+from ...Base.Events.Creation import *
+from ..TBClasses import *
+from ...Pulmonary.Node.BronchopulmonarySegment import *
+from ...Pulmonary.Node.BronchialTreeNode import *
+from ...Pulmonary.Node.LymphNode import *
+
+__author__ = "Michael Pitcher"
+__copyright__ = "Copyright 2017"
+__credits__ = ["Michael Pitcher"]
+__license__ = ""
+__version__ = "1.0.8"
+__email__ = "mjp22@st-andrews.ac.uk"
+__status__ = "Development"
+
+
+class TCellHelperCloning(Replication):
+    def __init__(self, probability):
+        Replication.__init__(self, [BronchopulmonarySegment, BronchialTreeNode, LymphNode], probability,
+                             T_CELL_HELPER)
+
+
+class TCellCytotoxicCloning(Replication):
+    def __init__(self, probability):
+        Replication.__init__(self, [BronchopulmonarySegment, BronchialTreeNode, LymphNode], probability,
+                             T_CELL_CYTOTOXIC)
---
+++ v8_ComMeN/Test/Test_TB/Test_TBComp_TCellClone.py
@@ -0,0 +1,25 @@
+import unittest
+
+from v8_ComMeN.ComMeN.TB.EventsWithCompartments.TCellCloning import *
+
+
+class TCellHelperCloningTestCase(unittest.TestCase):
+    def setUp(self):
+        self.event = TCellHelperCloning(0.1)
+
+    def test_something(self):
+        self.assertItemsEqual(self.event.node_types, [BronchopulmonarySegment, BronchialTreeNode, LymphNode])
+        self.assertEqual(self.event.compartment_created, T_CELL_HELPER)
+
+
+class TCellCytotoxicCloningTestCase(unittest.TestCase):
+    def setUp(self):
+        self.event = TCellCytotoxicCloning(0.1)
+
+    def test_something(self):
+        self.assertItemsEqual(self.event.node_types, [BronchopulmonarySegment, BronchialTreeNode, LymphNode])
+        self.assertEqual(self.event.compartment_created, T_CELL_CYTOTOXIC)
+
+
+if __name__ == '__main__':
+    unittest.main()
