f594ee28d6fec3598e295de75350b1d5cb852f56
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-25 11:13:47 +0100

    Change events moved to base level

--- v8_ComMeN/ComMeN/Base/Events/Change.py
+++ v8_ComMeN/ComMeN/Base/Events/Change.py
@@ -19,27 +19,48 @@ __status__ = "Development"
 
 class Change(Event):
 
-    def __init__(self, node_types, probability, compartment_from, compartment_to):
+    def __init__(self, node_types, probability, compartment_from, compartment_to, internals_to_destroy=None):
         self.compartment_from = compartment_from
         self.compartment_to = compartment_to
+        self.internals_to_destroy = internals_to_destroy
         Event.__init__(self, node_types, probability)
 
     def increment_from_node(self, node, network):
         return node.subpopulations[self.compartment_from]
 
     def update_node(self, node, network):
+        if self.internals_to_destroy:
+            for compartment in self.internals_to_destroy:
+                amount = node.compartment_per_compartment(compartment, self.compartment_from)
+                node.update_subpopulation(compartment, -1 * amount)
         node.update_subpopulation(self.compartment_from, -1)
         node.update_subpopulation(self.compartment_to, 1)
 
 
-class ChangeDestroyInternals(Change):
-    def __init__(self, node_types, probability, compartment_from, compartment_to,
-                 internal_compartments):
-        self.internal_compartments = internal_compartments
-        Change.__init__(self, node_types, probability, compartment_from, compartment_to)
+class ChangeByOtherCompartments(Change):
 
-    def update_node(self, node, network):
-        for c in self.internal_compartments:
-            amount = node.compartment_per_compartment(c, self.compartment_from)
-            node.update_subpopulation(c, -1 * amount)
-        Change.update_node(self, node, network)
+    def __init__(self, node_types, probability, compartment_from, compartment_to, external_compartments,
+                 internals_to_destroy=None):
+        self.influencing_compartments = external_compartments
+        Change.__init__(self, node_types, probability, compartment_from, compartment_to, internals_to_destroy)
+
+    def increment_from_node(self, node, network):
+        return Change.increment_from_node(self, node, network) * \
+               sum([node.subpopulations[c] for c in self.influencing_compartments])
+
+
+class ChangeByLackOfOtherCompartments(Change):
+
+    def __init__(self, node_types, probability, compartment_from, compartment_to, external_compartments,
+                 internals_to_destroy=None):
+        self.influencing_compartments = external_compartments
+        Change.__init__(self, node_types, probability, compartment_from, compartment_to, internals_to_destroy)
+
+    def increment_from_node(self, node, network):
+        # TODO - check this: epsilon = low number
+        epsilon = 0.00000001
+        number_externals = sum([node.subpopulations[c] for c in self.influencing_compartments])
+        if number_externals == 0:
+            return Change.increment_from_node(self, node, network) * (1 / epsilon)
+        else:
+            return Change.increment_from_node(self, node, network) * (1 / number_externals)
--- v8_ComMeN/Test/Test_Base/Test_change.py
+++ v8_ComMeN/Test/Test_Base/Test_change.py
@@ -8,11 +8,15 @@ class ChangeTestCase(unittest.TestCase):
     def setUp(self):
         self.comp_from = 'a'
         self.comp_to = 'b'
+        self.internal_comps = ['c', 'd']
         self.event = Change(None, 0.1, self.comp_from, self.comp_to)
+        self.event_with_internals = Change(None, 0.1, self.comp_from, self.comp_to, self.internal_comps)
 
     def test_initialise(self):
         self.assertEqual(self.event.compartment_from, self.comp_from)
         self.assertEqual(self.event.compartment_to, self.comp_to)
+        self.assertFalse(self.event.internals_to_destroy)
+        self.assertItemsEqual(self.event_with_internals.internals_to_destroy, self.internal_comps)
 
     def test_increment_from_node(self):
         node = Patch(0, [self.comp_from, self.comp_to])
@@ -27,29 +31,59 @@ class ChangeTestCase(unittest.TestCase):
         self.assertEqual(node.subpopulations[self.comp_from], 5)
         self.assertEqual(node.subpopulations[self.comp_to], 1)
 
-
-class ChangeDestroyInternalsTestCase(unittest.TestCase):
-    def setUp(self):
-        self.comp_from = 'a'
-        self.comp_to = 'b'
-        self.internal_comps = ['c','d']
-        self.event = ChangeDestroyInternals(None, 0.1, self.comp_from, self.comp_to, self.internal_comps)
-
-    def test_initialise(self):
-        self.assertEqual(self.event.compartment_from, self.comp_from)
-        self.assertEqual(self.event.compartment_to, self.comp_to)
-        self.assertItemsEqual(self.event.internal_compartments, self.internal_comps)
-
-    def test_update_node(self):
         node = Patch(0, [self.comp_from, self.comp_to, self.internal_comps[0], self.internal_comps[1]])
         node.update_subpopulation(self.comp_from, 6)
         node.update_subpopulation(self.internal_comps[0], 12)
         node.update_subpopulation(self.internal_comps[1], 33)
-        self.event.update_node(node, None)
+        self.event_with_internals.update_node(node, None)
         self.assertEqual(node.subpopulations[self.comp_from], 5)
         self.assertEqual(node.subpopulations[self.comp_to], 1)
         self.assertEqual(node.subpopulations[self.internal_comps[0]], 10)
         self.assertEqual(node.subpopulations[self.internal_comps[1]], 28)
+
+
+class ChangeByExternalsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.comp_from = 'mac_a'
+        self.comp_to = 'mac_b'
+        self.externals = ['inf_a', 'inf_b']
+        self.event = ChangeByOtherCompartments(None, 0.1, self.comp_from, self.comp_to, self.externals)
+
+    def test_initialise(self):
+        self.assertItemsEqual(self.event.influencing_compartments, self.externals)
+
+    def test_increment_from_node(self):
+        node = Patch(0, [self.comp_from, self.comp_to] + self.externals)
+        self.assertEqual(self.event.increment_from_node(node, None), 0)
+        node.update_subpopulation(self.comp_from, 10)
+        self.assertEqual(self.event.increment_from_node(node, None), 0)
+        node.update_subpopulation(self.externals[0], 1)
+        self.assertEqual(self.event.increment_from_node(node, None), 10 * 1)
+        node.update_subpopulation(self.externals[1], 2)
+        self.assertEqual(self.event.increment_from_node(node, None), 10 * (1+2))
+
+
+class ChangeByLackOfExternalsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.comp_from = 'mac_a'
+        self.comp_to = 'mac_b'
+        self.inf_comps = ['inf_a', 'inf_b']
+        self.event = ChangeByLackOfOtherCompartments(None, 0.1, self.comp_to, self.comp_from, self.inf_comps)
+
+    def test_initialise(self):
+        self.assertItemsEqual(self.event.influencing_compartments, self.inf_comps)
+
+    def test_increment_from_node(self):
+        node = Patch(0, [self.comp_from, self.comp_to] + self.inf_comps)
+        self.assertEqual(self.event.increment_from_node(node, None), 0)
+        node.update_subpopulation(self.comp_to, 10)
+        self.assertEqual(self.event.increment_from_node(node, None), 10 * (1 / 0.00000001))
+        node.update_subpopulation(self.inf_comps[0], 1)
+        self.assertEqual(self.event.increment_from_node(node, None), 10 * (1 / 1))
+        node.update_subpopulation(self.inf_comps[1], 2)
+        self.assertEqual(self.event.increment_from_node(node, None), 10 * (1 / (1+2)))
 
 if __name__ == '__main__':
     unittest.main()
