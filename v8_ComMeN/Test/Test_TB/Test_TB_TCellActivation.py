35eae7ea6dda1361b7104b66932a41195cc3a4be
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-04-27 10:25:50 +0100

    T Cell activation tests

--- v8_ComMeN/Test/Test_TB/Test_TB_TCellActivation.py
+++ v8_ComMeN/Test/Test_TB/Test_TB_TCellActivation.py
@@ -7,7 +7,7 @@ class TCellActivationHelperTestCase(unittest.TestCase):
     def setUp(self):
         self.event = TCellActivationHelper(0.1)
 
-    def test_something(self):
+    def test_initialise(self):
         self.assertEqual(self.event.compartment_from, T_CELL_NAIVE_HELPER)
         self.assertEqual(self.event.compartment_to, T_CELL_HELPER)
         self.assertItemsEqual(self.event.influencing_compartments, [MACROPHAGE_INFECTED])
@@ -17,7 +17,7 @@ class TCellActivationCytotoxicTestCase(unittest.TestCase):
     def setUp(self):
         self.event = TCellActivationCytotoxic(0.1)
 
-    def test_something(self):
+    def test_initialise(self):
         self.assertEqual(self.event.compartment_from, T_CELL_NAIVE_CYTOTOXIC)
         self.assertEqual(self.event.compartment_to, T_CELL_CYTOTOXIC)
         self.assertItemsEqual(self.event.influencing_compartments, [MACROPHAGE_INFECTED])
