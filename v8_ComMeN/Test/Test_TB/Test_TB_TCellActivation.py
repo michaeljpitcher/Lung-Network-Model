6ef689681274efad0557a5f9df2403e887a3258f
michaeljpitcher <mjp22@st-andrews.ac.uk>
2017-05-02 13:08:33 +0100

    Rename module

--- v8_ComMeN/Test/Test_TB/Test_TB_TCellActivation.py
+++ v8_ComMeN/Test/Test_TB/Test_TB_TCellActivation.py
@@ -1,6 +1,6 @@
 import unittest
 
-from v8_ComMeN.ComMeN.TB.Events.TCellPriming import *
+from v8_ComMeN.ComMeN.TB.Events.PhagocytePrimesTCell import *
 
 
 class TCellHelperActivationDendriticTestCase(unittest.TestCase):
